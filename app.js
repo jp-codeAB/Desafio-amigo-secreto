f88ac7079c251ed9d48fd7fc22475efbb6c00001
Castañeda <juxnpxblojpcd315@gmail.com>
2025-08-25 00:13:52 -0500

    Cree función para agregar amigos y limpiar el imput

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 //Mi Codigo para el Challenge del Amigo Secreto
 
-let amigoSecreto = [] //Array que almacenara los nombres de los amigos ingresados
+let amigoSecreto = []; // Array que almacenará los nombres de los amigos ingresados
+
+function agregarAmigo() {
+    let amigoIngresado = document.getElementById('amigoSecreto').value; // Capturar valor en el momento
+
+    if (amigoIngresado.trim() === "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigoSecreto.push(amigoIngresado.trim()); // Guardar sin espacios extra
+        console.log(amigoSecreto);
+        limpiarCaja();
+    }
+}
+
+// Limpiar la caja cada que se pulse Nuevo juego
+function limpiarCaja() {
+    document.querySelector('#amigoSecreto').value = '';
+}
